850d14853c31577449de871db6cb7b01819ce55a
Abhishek Tiwari <18tiwariabhishek@gmail.com>
2021-06-09 19:23:56 +0530

    Added PROFILE API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,19 @@ from profiles_api import serializers
 
 from rest_framework import viewsets
 
+from profiles_api import models
+
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
 
 
 class HelloApiView(APIView):
